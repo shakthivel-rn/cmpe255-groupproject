2a53a0764c492fd3e91b0e5625c2f31565a3a2fc
DivyaMohan <divyamohan94@gmail.com>
2021-05-10 09:19:03 -0700

    Committed code for hyper parameters tuning

--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -112,5 +112,44 @@ rf = RandomForestClassifier(n_estimators = 20, criterion = 'entropy', max_depth=
 rf.fit(x_tr, y_train)
 model_Evaluate(rf)
 
+#HypeR parameters TUNING using Random grid search CV
+
+from sklearn.model_selection import RandomizedSearchCV
+
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4,10]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestClassifier()
+# Random search of parameters, using 3 fold cross validation, 
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# Fit the random search model
+rf_random.fit(x_tr, y_train)
+
+print('rf base param',rf_random.best_params_)
+
+rf = RandomForestClassifier(n_estimators = 400, min_samples_split=10,min_samples_leaf=1,max_features='sqrt',max_depth= 100, bootstrap= True,random_state = 42)
+rf.fit(x_tr, y_train)
+model_Evaluate(rf)
+
 
 joblib.dump(rf, 'RandomForest.pkl')
